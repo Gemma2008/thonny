9cdabea2e0949ef61846e796e2feeea7ecd265e6
Gemma2008 <114920831+Gemma2008@users.noreply.github.com>
2023-03-13 13:15:04 +0100

    Create thonny.py

---
+++ thonny.py
@@ -0,0 +1,9 @@
+#Realizar un programa en phyton que solicite dos números y
+#luego mostrar el resultado
+
+num1 = int(input("Dime un numero: "))
+num2 = int(input("Dime otro numero: "))
+res = num1 + num2
+print("El resultado es: " , res)
+
+
