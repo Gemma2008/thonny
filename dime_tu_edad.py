b50f99206b0c43b6256e1739df183913689a855b
Gemma2008 <114920831+Gemma2008@users.noreply.github.com>
2023-03-13 13:18:23 +0100

    Create dime_tu_edad.py

---
+++ dime_tu_edad.py
@@ -0,0 +1,10 @@
+#Realizar un programa en phyton que solicite dos números y
+#luego mostrar el resultado
+
+edad = int(input("Dime tu edad "))
+
+if( edad >= 18):
+    print("es mayor")
+else:
+    print("es menor")
+
